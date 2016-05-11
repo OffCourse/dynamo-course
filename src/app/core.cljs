328ed28ea783312521fa108ad0b81934391a23a1
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-05-11 16:58:00 +0200

    fix problem

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,5 +1,6 @@
 (ns app.core
   (:require [cljs.nodejs :as nodejs]
+            [clojure.walk :as walk]
             [cljs.core.async :refer [<! chan >!]])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
@@ -8,12 +9,45 @@
 (def doc (nodejs/require "dynamodb-doc"))
 (def dynamo (.DynamoDB doc))
 
+(defn convert-js [event]
+  (-> event
+      js->clj
+      walk/keywordize-keys))
+
+
+(defn handle-db-response [response]
+  (-> response
+      js->clj
+      walk/keywordize-keys
+      :Items))
+
 (defn get-courses []
   (let [c (chan)
         query {:TableName "courses"}]
-    (.scan dynamo (clj->js query) #(go (>! c (clj->js %2))))
+    (.scan dynamo (clj->js query) #(go (>! c (handle-db-response %2))))
     c))
 
-(defn handler [event context cb]
+(defmulti get-data
+  (fn [{:keys [type] :as event}] (keyword type)))
+
+(defmethod get-data :courses [{:keys [type] :as event}]
   (go
-    (cb nil (<! (get-courses)))))
+    {:type :courses
+     type (<! (get-courses))}))
+
+(defmethod get-data :collection [{:keys [type collection] :as event}]
+  (go
+    (let [courses (<! (get-courses))
+          ids (map :course-id courses)
+          collection (assoc collection :course-ids ids)]
+      {:type :collection
+       type collection})))
+
+(defn ^:export handler [event context cb]
+  (go
+    (let [{:keys [type] :as event} (convert-js event)
+          data (<! (get-data event))]
+      (cb nil (clj->js data)))))
+
+(defn -main [] identity)
+(set! *main-cli-fn* -main)
